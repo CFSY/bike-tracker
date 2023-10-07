f34c6ca799c1afab42dd9f6d6091514c140467b1
Clement Foo <e0726745@u.nus.edu>
2023-10-07 19:18:28 +0800

    Add acceleration

--- src/components/tracker.jsx
+++ src/components/tracker.jsx
@@ -3,11 +3,55 @@ import Geolocation from "@react-native-community/geolocation";
 
 function Tracker({ data }) {
   const [armed, setArmed] = useState(false);
+  const [theft, setTheft] = useState(false);
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
 
+  // Accelerometer data
+  let previousAcceleration = { x: 0, y: 0, z: 0 };
+  let jerkThreshold = 5;  // Set your own threshold for what you consider a jerk
+
+  // Detects a significant jerk in the device
+  function detectJerk(event) {
+    console.log(event)
+    // const a = event.accelerationIncludingGravity;
+    
+    // // Calculate the magnitude of the acceleration vector
+    // let accelerationMagnitude = Math.sqrt(a.x * a.x + a.y * a.y + a.z * a.z);
+    
+    // // Calculate the difference in acceleration
+    // let deltaAcceleration = Math.abs(accelerationMagnitude - previousAcceleration);
+    
+    // // Update previous acceleration
+    // previousAcceleration = accelerationMagnitude;
+    
+    // // If the change in acceleration is greater than the threshold, return true
+    // if (deltaAcceleration > jerkThreshold) {
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+  }
+
   useEffect(() => {
+    if (window.DeviceMotionEvent) {
+      window.addEventListener('devicemotion', detectJerk, true);
+    } else {
+      console.log("DeviceMotionEvent is not supported");
+    }
+
     if (armed) {
+      const intervalId = setInterval(() => {
+        if (detectJerk()) {
+          setTheft(true);
+        }
+      }, 1000);
+      return () => clearInterval(intervalId);
+    }
+  }, [armed]);
+
+  useEffect(() => {
+    if (theft) {
       const intervalId = setInterval(() => {
         Geolocation.getCurrentPosition((position) => {
           const { latitude, longitude } = position.coords;
@@ -16,10 +60,9 @@ function Tracker({ data }) {
           console.log(latitude, longitude);
         });
       }, 1000);
-
-      return () => clearInterval(intervalId); // This clears the interval when the component unmounts or when `armed` changes
+      return () => clearInterval(intervalId);
     }
-  }, [armed]);
+  }, [theft]);
 
   return (
     <div className="flex flex-col items-center justify-between bg-white rounded shadow-md p-3 h-[50vh] w-[90vw]">
@@ -28,7 +71,7 @@ function Tracker({ data }) {
         <h1 className="text-2xl font-bold mb-3">
           Status: {armed ? "Armed" : "Disarmed"}
         </h1>
-        {armed && (
+        {theft && (
           <div>
             <h1 className="text-xl mb-3">Latitude: {latitude}</h1>
             <h1 className="text-xl mb-3">Longitude: {longitude}</h1>
